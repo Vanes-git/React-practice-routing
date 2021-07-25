a93bd6a6853e335d8a700f5c3b3a9869d0b1d057
Ioann <vanes.sysoev@gmail.com>
2021-07-25 15:23:02 +0300

    feat: links
    for all qoutes and new qoute

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
 import QouteDetail from "./pages/QouteDetail";
 import NewQoute from "./pages/NewQoute";
+import Layout from "./components/layout/Layout";
 
 function App() {
     return (
-        <Switch>
-            <Route path='/' exact>
-                <Redirect to='/qoutes'/>
-            </Route>
-            <Route path='/qoutes' exact>
-                <AllQuotes />
-            </Route>
-            <Route path='/qoutes/:qouteId'>
-                <QouteDetail />
-            </Route>
-            <Route path='/new-qoute'>
-                <NewQoute />
-            </Route>
-        </Switch>
+        <Layout>
+            <Switch>
+                <Route path='/' exact>
+                    <Redirect to='/qoutes' />
+                </Route>
+                <Route path='/qoutes' exact>
+                    <AllQuotes />
+                </Route>
+                <Route path='/qoutes/:qouteId'>
+                    <QouteDetail />
+                </Route>
+                <Route path='/new-qoute'>
+                    <NewQoute />
+                </Route>
+            </Switch>
+        </Layout>
     );
 }
 
